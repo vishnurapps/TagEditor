24d7a43fd9a0cc83ebeb3c558e8bcde61dad7e83
Vishnu R <vishnurapps@gmail.com>
2018-06-16 23:05:52 +0530

    Added logging module and configuration

--- TagEditor.py
+++ TagEditor.py
@@ -1,25 +1,36 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import  *
 import sys
 import eyed3
- 
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s ::  %(name)s :: %(funcName)s :: %(levelname)s :: %(message)s')
+file_handler = logging.FileHandler("run.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
 class TagEditor(QDialog):
 
     def openAndLoad(self):
-        print("Inside the openAndLoad")
+        logger.debug("Inside the TagEditor openAndLoad")
         filename, filter= QFileDialog.getOpenFileName(parent=self, caption='Open file', filter='*.mp3')
-        print(filename)
+        logger.debug(filename)
         self.audiofile = eyed3.load(filename)
         self.leTrackNumber.setText(str(self.audiofile.tag.track_num[0]))
         self.leTitle.setText(self.audiofile.tag.title)
         self.leArtist.setText(self.audiofile.tag.artist)
         self.leAlbumArtist.setText(self.audiofile.tag.album_artist)
-        print(type(self.audiofile.tag.genre))
+        logger.debug(type(self.audiofile.tag.genre))
         self.leGenre.setText(str(self.audiofile.tag.genre))
         self.btnApply.setEnabled(True)
         self.btnOpen.setEnabled(False)
 
     def cancelAndClear(self):
+        logger.debug("Inside TagEditor cancelAndClear")
         self.leTrackNumber.clear()
         self.leTitle.clear()
         self.leArtist.clear()
@@ -28,6 +39,7 @@ class TagEditor(QDialog):
         self.btnOpen.setEnabled(True)
 
     def applyChanges(self):
+        logger.debug("Inside TagEditor applyChanges")
         self.audiofile.tag.track_num = int(self.leTrackNumber.text())
         self.audiofile.tag.title = self.leTitle.text()
         self.audiofile.tag.artist = self.leArtist.text()
@@ -37,7 +49,7 @@ class TagEditor(QDialog):
 
     def __init__(self):
         QDialog.__init__(self)
-
+        logger.debug("Inside TagEditor __init__")
         self.setWindowTitle("Mp3 Tag Editor")
         self.lblTrackNumber = QLabel("Track Number :")
         self.leTrackNumber = QLineEdit()
