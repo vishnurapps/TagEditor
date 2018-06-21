de03c5ec8f7b0bf0b02e5cb6c83635ad2b6c1102
Vishnu R <vishnurapps@gmail.com>
2018-06-21 23:51:50 +0530

    Added option to enter url to download image

--- ArtChanger.py
+++ ArtChanger.py
@@ -1,7 +1,7 @@
 import logging
-from PyQt5.QtWidgets import QPushButton, QHBoxLayout, QDialog, QFileDialog
+from PyQt5.QtWidgets import QPushButton, QHBoxLayout, QVBoxLayout, QDialog, QFileDialog, QLineEdit, QGridLayout
 from PyQt5.QtGui import QPixmap
-import webbrowser
+from subprocess import PIPE, Popen
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -21,31 +21,32 @@ class ArtChanger(QDialog):
         self.btnWeb = QPushButton("From Web")
         self.btnLocal = QPushButton("From Local")
         self.btnCancel = QPushButton("Cancel")
-        layout = QHBoxLayout()
-        layout.addWidget(self.btnWeb)
-        layout.addWidget(self.btnLocal)
-        layout.addWidget(self.btnCancel)
+        self.leImageBox = QLineEdit()
+        self.leImageBox.setPlaceholderText("Enter image url")
+        self.buttonLayout = QGridLayout()
+        self.buttonLayout.addWidget(self.btnWeb, 0, 0)
+        self.buttonLayout.addWidget(self.btnLocal, 0, 1)
+        self.buttonLayout.addWidget(self.btnCancel, 0, 2)
         logger.debug("Layout created and elements added")
         self.btnWeb.clicked.connect(self.from_web)
         self.btnLocal.clicked.connect(self.from_local)
         self.btnCancel.clicked.connect(self.cancel_and_close)
-        self.setLayout(layout)
+        self.setLayout(self.buttonLayout)
         logger.debug("Buttons connected")
 
     def from_web(self):
         logger.debug("Inside ArtChanger from_web")
-        base_url = "https://www.google.com/search?q="
-        search_term = self.album.replace(" ", "+")
-        webbrowser.open(base_url+search_term, autoraise=True)
+        self.buttonLayout.addWidget(self.leImageBox, 1, 0, 2, 0)
 
     def from_local(self):
         logger.debug("Inside ArtChanger from_local")
         filename, filter= QFileDialog.getOpenFileName(parent=self, caption='Open file', filter='*.jpg, *.jpeg, *.png')
         pixmap = QPixmap(filename)
+        copy = Popen(['cp', filename, "/tmp/sample.jpg"], stdout=PIPE)
+        copy.communicate()
         pixmap = pixmap.scaledToHeight(240)
         pixmap = pixmap.scaledToWidth(240)
         self.art.setPixmap(pixmap)
-        #self.art.q
 
     def cancel_and_close(self):
         logger.debug(("Inside ArtChanger cancel_and_close"))
